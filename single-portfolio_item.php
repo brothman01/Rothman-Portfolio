ad37e0a491e6320e23a2a316a83a1b53e0008be0
Ben Rothman <Ben@BenRothman.org>
2023-03-26 12:32:09 -0400

    fixed portfolio pages to only show thumbnail boxes if there are thumbnails and to show the first one if there is more than one.

--- single-portfolio_item.php
+++ single-portfolio_item.php
@@ -30,27 +30,47 @@
 				endwhile; // End of the loop.
 			?>
 
+	<?php
+	$images = array();
+	array_push( $images, get_post_meta( get_the_ID(), 'Brothman_Portfolio_image1', true ) );
+	array_push( $images, get_post_meta( get_the_ID(), 'Brothman_Portfolio_image2', true ) );
+	array_push( $images, get_post_meta( get_the_ID(), 'Brothman_Portfolio_image3', true ) );
+	array_push( $images, get_post_meta( get_the_ID(), 'Brothman_Portfolio_image4', true ) );
+	array_push( $images, get_post_meta( get_the_ID(), 'Brothman_Portfolio_image5', true ) );
+	array_push( $images, );
+	?>
+
 	<div class="bp_singlepage_selector_image_row" style="width:100%; margin-bottom: 3%;">
 
+		<?php if ( $images[0] != NULL && count( array_filter($images ) ) > 1 ) { ?>
 		<div class="bp_singlepage_thumbnail_div">
-			<img class="bp_portfolio_item_selector_image bp_portfolio_item_selected_image" src="<?php echo get_post_meta( get_the_ID(), 'Brothman_Portfolio_image1', true );?> " />
+			<img class="bp_portfolio_item_selector_image bp_portfolio_item_selected_image" src="<?php echo $images[0]; ?> " />
 		</div>
+		<?php } ?>
+		
+		<?php if ( $images[1] != NULL ) { ?>
+			<div class="bp_singlepage_thumbnail_div">
+			<img class="bp_portfolio_item_selector_image" src="<?php echo $images[1];?> " />
+		</div>
+		<?php } ?>
 
-		<div class="bp_singlepage_thumbnail_div">
-			<img class="bp_portfolio_item_selector_image" src="<?php echo get_post_meta( get_the_ID(), 'Brothman_Portfolio_image2', true );?> " />
+		<?php if ( $images[2] != NULL ) { ?>
+			<div class="bp_singlepage_thumbnail_div">
+			<img class="bp_portfolio_item_selector_image" src="<?php echo $images[2]; ?> " />
 		</div>
+		<?php } ?>
 
-		<div class="bp_singlepage_thumbnail_div">
-			<img class="bp_portfolio_item_selector_image" src="<?php echo get_post_meta( get_the_ID(), 'Brothman_Portfolio_image3', true );?> " />
+		<?php if ( $images[3] != NULL ) { ?>
+			<div class="bp_singlepage_thumbnail_div">
+			<img class="bp_portfolio_item_selector_image" src="<?php echo $images[3]; ?> " />
 		</div>
+		<?php } ?>
 
+		<?php if ( $images[4] != NULL ) { ?>
 		<div class="bp_singlepage_thumbnail_div">
-			<img class="bp_portfolio_item_selector_image" src="<?php echo get_post_meta( get_the_ID(), 'Brothman_Portfolio_image4', true );?> " />
+			<img class="bp_portfolio_item_selector_image" src="<?php echo $images[4]; ?> " />
 		</div>
-
-		<div class="bp_singlepage_thumbnail_div">
-			<img class="bp_portfolio_item_selector_image" src="<?php echo get_post_meta( get_the_ID(), 'Brothman_Portfolio_image5', true );?> " />
-		</div>
+		<?php } ?>
 
 	</div>
 
