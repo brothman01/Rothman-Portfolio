11b36c103a2651b998f02315fad5fb38821d8a68
Ben Rothman <Ben@BenRothman.org>
2023-05-18 13:52:06 -0400

    feature branch added + block scaffolded

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
 	require_once dirname( __FILE__ ) . '/cmb2/init.php';
 } elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
 	require_once dirname( __FILE__ ) . '/CMB2/init.php';
-}
+} 
 
 /**
  * Conditionally displays a metabox when used as a callback in the 'show_on_cb' cmb2_box parameter
