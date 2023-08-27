3daa5bb53cf5f889fe6fd3679b0ddabacb636182
Ben Rothman <Ben@BenRothman.org>
2023-08-27 15:02:37 -0400

    main-spinner: Added timed spinner before items load.

--- wordpress-block-react/build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'de69fc819c4e63e7d858');
--- wordpress-block-react/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5fbe1d264c68cee76957');
